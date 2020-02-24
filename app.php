7dc6b5ad4086511d84aff3527ac759b3f34a36ae
Kanat Gailimov <gailimov@gmail.com>
2020-02-24 10:11:36 +0600

    Stats removed

--- app.php
+++ app.php
@@ -4,9 +4,6 @@ use app\{App, Worker};
 
 error_reporting(E_ALL);
 
-$startTime = date('H:i:s');
-$startMicroTime = microtime(true);
-
 require_once 'vendor/autoload.php';
 
 $container = (new App(require_once 'config.php'))->initDependencies();
@@ -23,8 +20,3 @@ switch ($argv[1]) {
     default:
         echo "Undefined command\n";
 }
-
-echo 'Done' . PHP_EOL;
-echo 'Started at ' . $startTime . ', finished at ' . date('H:i:s') . PHP_EOL;
-echo 'Execution time is: ' . round((microtime(true) - $startMicroTime)) . ' sec' . PHP_EOL;
-echo 'Peak memory is: ' . round(memory_get_peak_usage() / (1024 * 1024), 2) . 'MB' . PHP_EOL;
